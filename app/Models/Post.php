7d7c371e1fa34e595a52c288623ae80276139562
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-01 13:14:26 +0500

    созданы отношения между Публикациями Пользователями и Категориями

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'thumbnail',
+        'body',
+        'active',
+        'poblished_at',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function categoties()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
